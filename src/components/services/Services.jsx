460fcc7af475fa5a8fd301bfe7e00d0485575003
ntuiraoul1 <134698011+ntuiraoul1@users.noreply.github.com>
2023-06-06 18:05:10 +0100

    Commited

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import './services.css'
+import {BiCheck} from 'react-icons/bi'
+
+const Services = () => {
+  return (
+    <section id='services'>
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Designer</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>User Interface</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>User Experience</p>
+            </li>
+         
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>User Interface</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>Brand identity Design</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>User Interface</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF UI/UX FIELD */}
+         <article className="service">
+          <div className="service__head">
+            <h3>Web development</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>User Interface</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>User Experience</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>SEO</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>Hosting</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>Boosting</p>
+            </li>
+         
+          </ul>
+        </article>
+            {/* WEB DEVELOPMENT ENDS*/}
+         <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>Social media content</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>User Experience</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>Content writting</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>Social branding</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'></BiCheck>
+              <p>Blogger</p>
+            </li>
+          
+          </ul>
+        </article>
+      </div>
+    </section>
+  )
+}
+
+export default Services
